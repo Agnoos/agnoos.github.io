d46c7b0390f56597ecf9f3db7667f971132dddae
Lucas Ricardo <lucasricardolemes@gmail.com>
2020-07-28 00:50:59 -0300

    Updates

---
+++ precache-manifest.5ba4e9e185289c977789c58e8c771937.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28540d9dd6cd295f473c9d58b40672a",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "ff02c376a1f865d1c3cf",
+    "url": "/site/static/css/2.10325ddc.chunk.css"
+  },
+  {
+    "revision": "9dc6123f0dd8b9e47d9b",
+    "url": "/site/static/css/main.b8d77f55.chunk.css"
+  },
+  {
+    "revision": "ff02c376a1f865d1c3cf",
+    "url": "/site/static/js/2.29f69932.chunk.js"
+  },
+  {
+    "revision": "5c639e0eadf54404f50f772674630a25",
+    "url": "/site/static/js/2.29f69932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc6123f0dd8b9e47d9b",
+    "url": "/site/static/js/main.84112801.chunk.js"
+  },
+  {
+    "revision": "1c0aea058aa1e7f7a27b",
+    "url": "/site/static/js/runtime-main.849a864b.js"
+  },
+  {
+    "revision": "4c62644b159530ff6214336f8cfb1d32",
+    "url": "/site/static/media/Futura Bold font.4c62644b.ttf"
+  },
+  {
+    "revision": "7921c032c04c08239056f438c30c7ceb",
+    "url": "/site/static/media/White.7921c032.png"
+  },
+  {
+    "revision": "f019b24059c232a14900ddbe493cb56b",
+    "url": "/site/static/media/admetec.f019b240.jpg"
+  },
+  {
+    "revision": "e1a27147bd5e14c63e49c3296a2cf35d",
+    "url": "/site/static/media/camt.e1a27147.png"
+  },
+  {
+    "revision": "bf67146bee9ddece720e869819bb63a4",
+    "url": "/site/static/media/customcandy.bf67146b.jpg"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/site/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/site/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/site/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/site/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/site/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/site/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/site/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/site/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/site/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/site/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/site/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/site/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/site/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/site/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/site/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/site/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/site/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/site/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/site/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/site/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3a25dbbc7d26163a4a67306346917a82",
+    "url": "/site/static/media/ibm.3a25dbbc.jpg"
+  },
+  {
+    "revision": "c66647caf54ba9688bb2b0f10476a67b",
+    "url": "/site/static/media/me.c66647ca.jpg"
+  },
+  {
+    "revision": "bbedb3df756ed8972f0d0e060afe5e83",
+    "url": "/site/static/media/psd.bbedb3df.png"
+  },
+  {
+    "revision": "72f8101f3cd369b72d668cbd24da30d4",
+    "url": "/site/static/media/pucc.72f8101f.jpg"
+  },
+  {
+    "revision": "b8435e8eebeca2310398010e1909eadd",
+    "url": "/site/static/media/unicamp.b8435e8e.jpg"
+  }
+]);
