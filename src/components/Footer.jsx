6b13ca7822d6f8f3581965117bf68dd13fb01f86
Agnoos <lucasricardolemes@gmail.com>
2022-03-27 00:24:34 -0300

    att

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ export default props =>
         <div className="container">
             <div class="mt-3">
                 <ul class="">
-                    <li class="list-inline-item"><a href="https://twitter.com/Agnoos_" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                    {/* <li class="list-inline-item"><a href="https://twitter.com/Agnoos_" target="_blank"><i class="fab fa-twitter"></i></a></li> */}
                     <li class="list-inline-item"><a href="https://github.com/Agnoos" target="_blank"><i class="fab fa-github"></i></a></li>
                     <li class="list-inline-item"><a href="https://www.linkedin.com/in/ricardo-lucass/" target="_blank"><i class="fab fa-linkedin" ></i></a></li>
                 </ul>
                 <div className="align-footer  mb-5">
-                    <p class="lucas-direitos ">© All right Reversed.<a class=" h6 ml-2 mb-0 lucas " href="https://twitter.com/Agnoos_" target="_blank">Lucas Ricardo</a></p>
+                    <p class="lucas-direitos ">© All right Reversed.<a class=" h6 ml-2 mb-0 lucas " href="https://www.linkedin.com/in/ricardo-lucass/" target="_blank">Lucas Ricardo</a></p>
                 </div>
             </div>
         </div>
